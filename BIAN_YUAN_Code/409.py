8fa9c30c4fcf4d92f5de08bb23e66fe19eadf78f
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-02-13 16:12:03 -0500

    Create 409.py

---
+++ BIAN_YUAN_Code/409.py
@@ -0,0 +1,32 @@
+#Note: we can take any combo of the letters!
+class Solution(object):
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+    
+        dic_count={}
+        for i in range(len(s)):
+            if s[i] in dic_count:
+                dic_count[s[i]]+=1
+            else:
+                dic_count[s[i]]=1
+    
+        valuelist=dic_count.values()
+    
+        even_list = [ele for ele in valuelist if ele%2==0]
+        odd_list  = [ele for ele in valuelist if ele%2!=0]
+    
+        if len(odd_list)==0:
+            return sum(even_list)
+            
+        elif len(even_list)==0:
+            return sum(odd_list)-len(odd_list)+1
+            
+        else:
+            return sum(even_list)+sum(odd_list)-len(odd_list)+1
+
+        
+        
