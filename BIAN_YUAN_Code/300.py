dc67f14399320f01fea5e723270dec5041ec9bda
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-02-20 15:29:03 -0500

    Create 300.py

---
+++ BIAN_YUAN_Code/300.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:
+            return 0
+        dic={}
+        dic[0]=1
+        maxlen=1
+        
+        for i in range(1,len(nums)):
+            #for j in range(i):
+                
+            dic[i]=max([dic[j] for j in range(i) if nums[i]>nums[j]]+[0])+1
+            
+
+            
+            if dic[i]>maxlen:
+                maxlen=dic[i]
+        
+        return maxlen
+        
