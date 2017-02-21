75e0f25cb7c21cdc1a3d1de5ccad2dabaeb1dced
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-02-20 23:31:14 -0500

    Create 62.py

---
+++ BIAN_YUAN_Code/62.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        d=[[None]*n for i in range(m)]#mxn
+        for i in range(n):
+            d[0][i]=1
+        for j in range(m):
+            d[j][0]=1
+        
+        for i in range(1,m):
+            for j in range(1, n):
+                d[i][j]=d[i-1][j]+d[i][j-1]
+                
+        return d[m-1][n-1]
+        
