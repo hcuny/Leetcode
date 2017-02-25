254dedd6f500bd5fbcc1013b7aca7f366116f424
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-02-24 21:12:44 -0500

    Create 357.py

---
+++ BIAN_YUAN_Code/357.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        Devide: e.g. 0-1000,d[0]--repeat in [0,10), d[1]=d[0]+repeat in [10,100),  etc..
+        """
+        if n<=1:
+            return 10**(n)
+            
+        d=[None]*(n+1)
+        
+        d[0]=10
+        
+        for i in range(1,n):
+            
+            d[i]=d[i-1]+9*math.factorial(9)/math.factorial(9-i)
+            
+        return d[n-1]
