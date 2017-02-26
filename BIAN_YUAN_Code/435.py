c2c2f3edb80991a876660fed939148f7ab59a4c8
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-02-26 00:59:38 -0500

    Update 435.py

--- BIAN_YUAN_Code/435.py
+++ BIAN_YUAN_Code/435.py
@@ -29,4 +29,19 @@ class Solution(object):
                 
         return count
                 
-        
+ #---------------------------------Compare to a Top solution-------------------------------------
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+        if not intervals: return 0
+        intervals.sort(key=lambda x: x.start)  # sort on start time
+        currEnd, cnt = intervals[0].end, 0
+        for x in intervals[1:]:
+            if x.start < currEnd:  # find overlapping interval
+                cnt += 1
+                currEnd = min(currEnd, x.end)  # erase the one with larger end time
+            else:
+                currEnd = x.end   # update end time
+        return cnt
