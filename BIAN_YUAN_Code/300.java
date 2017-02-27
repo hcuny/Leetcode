73a6d7f294a01458108a1f7ee2a6266a8414abca
zsmjhtn <470479582@qq.com>
2017-02-27 10:20:36 +0800

    补上注释

--- BIAN_YUAN_Code/300.java
+++ BIAN_YUAN_Code/300.java
@@ -19,11 +19,14 @@ public class Maths {
 		int[] dp = new int[nums.length];
 		int len = 0;
 		for (int x : nums) {
+			//二分查找，需要在排序好的数组里查找，这里我们是[0,0,0,0,0,0,0,0,......]
+			//取（0，len-1）的midindex的数组值，和x比较，如果x大，则mid右移一位，取（mid+1 , len-1）的中间值，如此循环比较
+			//如果x小，则mid左移一位，取（0 ， mid-1）的中间值，如此循环比较
 			int i = Arrays.binarySearch(dp, 0, len, x);
 			if (i < 0)
 				i = -(i + 1);
 			dp[i] = x;
-			if (i == len)
+			if (i == len)//如果（0，i）位置都被填满，那么数组长度 len = i+1。只有当已排序的数组里的任意元素都小于x时，len才会+1，在数组里给出一个允许放更大数的位置
 				len++;
 		}
 		return len;
--- BIAN_YUAN_Code/62.java
+++ BIAN_YUAN_Code/62.java
@@ -33,7 +33,7 @@ public class Maths {
 		}
 		for (int i = 1; i < large; i++) {
 			for (int j = 1; j < small; j++) {
-				cur[j] = cur[j - 1] + cur[j];
+				cur[j] = cur[j - 1] + cur[j];// 因为cur[0] === 1的性质，可以做到，计算一个覆盖一个cur[j]。
 			}
 		}
 		return cur[small - 1];
@@ -44,6 +44,12 @@ public class Maths {
 	 * 多了 3* max（m,n）的数组拷贝
 	 * @param large
 	 * @return
+	 * 思路： cur[i] = cur[i - 1] + pre[i];第一行第一列肯定全是1，因为就一种走法，不是连续下就是连续右。
+	 * pre[i]记录上一次的结果，第一次肯定是第二行、第二列  开始： cur[i] = cur[i - 1] + 1 ，然后把 cur[i]的数组赋给 pre[i]记录。
+	 * 之后进行递归。
+	 * 有个可以优化的思路：cur[i]在赋给pre[i]之前，存的是当前递推计算完的结果。接下来马上进行下一行的递推。而且第一个要递推出的是j=1，又cur[0] = 1，cur[1，i]存的是上一行的计算结果
+	 * 所以很容易得到cur[1]的结果：cur[1](这是上一行的结果) + 1。然后把最新的这一行的结果的值，覆盖了上一行的结果值cur[1]。
+	 * 之后cur[j]依次可以递推出，算出一个覆盖一个cur[j]。所以可以完全不需要缓存pre[]。省下内存空间。
 	 */
 	public static int uniquePaths2(int small, int large) {
 		int[] pre = new int[small];
