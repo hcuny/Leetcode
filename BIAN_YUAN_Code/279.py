aa30d3343e51d994f72571e7635722facbf61b38
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-03-03 16:40:33 -0500

    Update 279.py

--- BIAN_YUAN_Code/279.py
+++ BIAN_YUAN_Code/279.py
@@ -2,7 +2,8 @@
 # ---------------------------------------------------- Solution 1-----------------------------------------------------------
 """
 First thought--"use" the result of the last valid perfect square problem垃圾解法
-nearestPS(n) : return the nearest (less than n) perfect square number
+nearestPS(n) : return the nearest (less than n) perfect square number (其实是返回平方根)
+& Use traditional DP idea.
 """
 class Solution(object):
     def nearestPS(self, num):
@@ -16,17 +17,13 @@ class Solution(object):
         high=num
         mid=(low+high)//2
         
-        while mid**2>num or (mid+1)**2<=num:
-            
+        while mid**2>num or (mid+1)**2<=num:            
             if mid**2>num:
-    
                 high=mid
                 mid=(low+high)//2
             else:
                 low=mid
                 mid=(low+high)//2
-                
-
         return mid
     
     def numSquares(self, n):
@@ -46,3 +43,52 @@ class Solution(object):
             
         return dp[n]
         
+#---------------------------------------------- Solution 2 --------------------------------------------
+"""
+Pure DP solution, just a little modification
+"""
+
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp=[None]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        
+        if n<2:
+            return dp[n]
+        
+        for i in range(2,n+1):
+            min_value=i
+            j=1
+            while j**2<=i:
+                temp=dp[i-j**2]+1
+                if temp<min_value:
+                    min_value=temp
+                j+=1
+                
+            dp[i]=min_value
+            
+        return dp[n]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
