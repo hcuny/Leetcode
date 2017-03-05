c9171d50612d3d590cc4e53ea28f041093b5d811
Anthony Wong Chuen-yuen <hcy.apply@hotmail.com>
2017-03-05 09:06:40 -0500

    Update 179.py

--- BIAN_YUAN_Code/179.py
+++ BIAN_YUAN_Code/179.py
@@ -1,6 +1,10 @@
 class Solution:
     # @param {integer[]} nums
     # @return {string}
+    """
+    Rewrite Comparator
+    """
+    
     def largestNumber(self, nums):
         nums = map(str, nums)
         nums.sort(cmp=lambda a,b: cmp(a+b, b+a), reverse=True)
